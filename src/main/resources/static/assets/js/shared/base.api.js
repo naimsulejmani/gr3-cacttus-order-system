9a697931268f511510eea900d2846ab3d1c07067
Naim Sulejmani <naim.sulejmani@gmail.com>
2025-01-17 18:54:34 +0100

    axios things

--- src/main/resources/static/assets/js/shared/base.api.js
+++ src/main/resources/static/assets/js/shared/base.api.js
@@ -5,8 +5,11 @@ class BaseApi {
     }
 
     async findAll() {
-        const response = await fetch(this.baseUrl);
-        return response.json();
+        const response = await axios.get(this.baseUrl);
+        console.log(this.baseUrl, response);
+        return response.data;
+        // const response = await fetch(this.baseUrl);
+        // return response.json();
     }
 
     async findById(id) {
@@ -14,15 +17,19 @@ class BaseApi {
         return response.json();
     }
 
-    async add(entity){
-        const response = await fetch(this.baseUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(entity)
-        });
-        return response.json();
+    async add(entity) {
+        // const response = await fetch(this.baseUrl, {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(entity)
+        // });
+        // return response.json();
+        const response = await axios.post(this.baseUrl, JSON.stringify(entity),
+            {headers: {'Content-Type': 'application/json'}});
+        console.log(this.baseUrl, response);
+        return response.data;
     }
 
     async modify(id, entity) {
@@ -36,7 +43,7 @@ class BaseApi {
         return response.json();
     }
 
-    async removeById(id){
+    async removeById(id) {
         const response = await fetch(`${this.baseUrl}/${id}`, {
             method: 'DELETE'
         });
